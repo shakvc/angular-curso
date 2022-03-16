1f478a44123836060eddf14095b010717caabac8
Kelly Valdes <shakelly90@hotmail.com>
2022-03-16 16:39:40 -0500

    test 3

--- src/app/dbz/agregar/agregar.component.ts
+++ src/app/dbz/agregar/agregar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 import { DbzService } from '../services/dbz.services';
 
